a77c8a4df44b5c883710e34a19ee7d5206b4229a
Tyrel Souza <923113+tyrelsouza@users.noreply.github.com>
2019-08-28 17:01:45 -0400

    clean up file

--- conda_parser/info.py
+++ conda_parser/info.py
@@ -1,5 +1,4 @@
 from conda.api import Solver
-from conda.exceptions import ResolvePackageNotFound
 
 
 def package_info(channel: str, package: str, version: str) -> dict:
@@ -10,7 +9,8 @@ def package_info(channel: str, package: str, version: str) -> dict:
     packages = Solver(".", [channel], specs_to_add=[spec]).solve_final_state()
 
     # find the package passed in
-    record = dict([dep for dep in packages if dep.name == package][0].dump())
+    first_package_record = [dep for dep in packages if dep.name == package][0]
+    record = dict(first_package_record.dump())
 
     # Clear out some unneeded keys
     del record["arch"]
