ca686e9d64ed557b53ce85c03cdb652bda0ba183
Tyrel Souza <923113+tyrelsouza@users.noreply.github.com>
2019-08-28 17:02:26 -0400

    clear up some code

--- conda_parser/info.py
+++ conda_parser/info.py
@@ -8,7 +8,7 @@ def package_info(channel: str, package: str, version: str) -> dict:
     # solve the spec for this package.
     packages = Solver(".", [channel], specs_to_add=[spec]).solve_final_state()
 
-    # find the package passed in
+    # find the package passed in, it will be there, as Solver rasies if not found
     first_package_record = [dep for dep in packages if dep.name == package][0]
     record = dict(first_package_record.dump())
 
